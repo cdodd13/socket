54ecfdae9f758db76beb1b4c995b363dfd142414
cdodd13 <cdodd1195@gmail.com>
2018-04-06 04:26:20 +0000

    add client-side test code

---
+++ src/client.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+#define PORT 8080
+
+int findPrime(int previous);
+
+int main(int argc, char const *argv[]) {
+	struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    int buffer[1024] = {0};
+    if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "10.0.0.10", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection failed \n");
+        return -1;
+    }
+
+    int txVal = 1;
+    int rxVal = 1;
+    int count = 1;
+    while(count <= 100) {
+        txVal = findPrime(rxVal);
+        send(sock, &txVal, sizeof(txVal), 0);
+        printf("sent: %i\n", txVal);
+        valread = read(sock, buffer, 1024);
+        rxVal = *buffer;
+        count += 1;
+    }
+
+//    send(sock, hello, strlen(hello), 0);
+//    printf("Hello message sent\n");
+//    valread = read(sock, buffer, 1024);
+//    printf("%s\n", buffer);
+
+    return 0;
+}
+
+int findPrime(int previous) {
+    int flag = 1;
+    int n = previous;
+    while(flag) {
+        n += 1;
+        flag = 0;
+        for(int i = 2; i < n; i++) {
+            if(n % i == 0) {
+                flag = 1;
+                break;
+            }
+        }
+    }
+    return n;
+}
